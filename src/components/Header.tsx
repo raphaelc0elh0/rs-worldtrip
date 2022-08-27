1885794c20161464e2f236a93b1d3feffd120d8a
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-08-27 20:00:39 -0300

    feat: hander component

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react"
+
+export const Header = () => {
+  return (
+    <Flex align="center" justify="center" width="100%" height={100}>
+      <img src="/images/logo.svg" alt="logo" width={184} />
+    </Flex>
+  )
+}
